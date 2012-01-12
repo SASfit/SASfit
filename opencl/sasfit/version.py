d7a5029ae4943f4af89754389b7a13d64aaa044e
Ingo Bre?ler <ingo.bressler@bam.de>
2012-01-12 18:17:22 +0100

    updated hgignore
    - added missing, prev. ignored files

---
+++ opencl/sasfit/version.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# version.py
+# 
+# Copyright (c) 2010-2012 BAM
+# Federal Institute for Materials Research and Testing
+# Division I.3 Structure Analysis; Polymer Analysis
+# Richard-Willstaetter-Strasse 11, 12489 Berlin, Germany
+# 
+# This file is part of SASfit.
+#
+# SASfit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SASfit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SASfit.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Author(s) of this file:
+# Ingo Bressler (sasfit@ingobressler.net)
+
+from cutesnake import AppVersion
+
+version = AppVersion(
+    programName = "SASfit",
+    versionNumber = "2.0.0",
+    organizationName = "BAM"
+)
+
