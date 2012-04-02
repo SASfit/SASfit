507728809093f0b1481896b10725f30dd62be258
Kohlbrecher <joachim.kohlbrecher@psi.ch>
2012-04-02 16:46:45 +0200

    - extended the existing plugin for fuzzy spheres by a new one with a parabolic interface and a core shell structure. - extended the manual

--- src/plugins/anisotropic_objects/sasfit_ff_pcs_homogeneouscyl.c
+++ src/plugins/anisotropic_objects/sasfit_ff_pcs_homogeneouscyl.c
@@ -13,12 +13,73 @@
 #define DUMMY2	param->p[3]
 #define DUMMY3	param->p[4]
 #define DUMMY4	param->p[5]
-#define ETA_L	param->p[6]
-#define ETA_SOL	param->p[7]
+#define ETA_CORE	param->p[6]
+#define ETA_SOL		param->p[7]
 
+#define Q	param->p[MAXPAR-1]
+
+scalar homo_cyl_Ampl_core(scalar x, sasfit_param *param)
+{
+	scalar A,u;
+	u = Q*x;
+	if (u==0) {
+		A = M_PI*R*R*(ETA_CORE-ETA_SOL)*2;
+	} else {
+		A = M_PI*R*R*(ETA_CORE-ETA_SOL)*2*gsl_sf_bessel_J1(u)/u;
+	}
+	return A;
+}
+
+scalar homo_cyl_I_core(scalar x, sasfit_param *param)
+{
+	return gsl_pow_2(homo_cyl_Ampl_core(x,param));
+}
+
+scalar Ampl_homoXS_core(scalar x, sasfit_param *param) 
+{
+	scalar Pcs, u, LNdistr;
+	sasfit_param subParam;
+
+	SASFIT_ASSERT_PTR(param);
+
+	Pcs = homo_cyl_Ampl_core(x,param);
+	
+	sasfit_init_param( &subParam );
+	subParam.p[0] = 1.0;
+	subParam.p[1] = SIGMA_R;
+	subParam.p[2] = 1.0;
+	subParam.p[3] = R;
+
+	LNdistr = sasfit_sd_LogNorm(x, &subParam);
+	SASFIT_CHECK_SUB_ERR(param, subParam);
+
+	return LNdistr*Pcs;
+}
+
+scalar I_homoXS_core(scalar x, sasfit_param *param) 
+{
+	scalar Pcs, u, LNdistr;
+	sasfit_param subParam;
+
+	SASFIT_ASSERT_PTR(param);
+
+	Pcs = homo_cyl_I_core(x,param);
+	
+	sasfit_init_param( &subParam );
+	subParam.p[0] = 1.0;
+	subParam.p[1] = SIGMA_R;
+	subParam.p[2] = 1.0;
+	subParam.p[3] = R;
+
+	LNdistr = sasfit_sd_LogNorm(x, &subParam);
+	SASFIT_CHECK_SUB_ERR(param, subParam);
+
+	return LNdistr*Pcs;
+}
 
 scalar sasfit_ff_pcs_homogeneouscyl(scalar q, sasfit_param * param)
 {
+	scalar Pcs, Rstart, Rend;
 	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
 
 	SASFIT_CHECK_COND1((q < 0.0), param, "q(%lg) < 0",q);
@@ -26,15 +87,34 @@ scalar sasfit_ff_pcs_homogeneouscyl(scalar q, sasfit_param * param)
 	SASFIT_CHECK_COND1((SIGMA_R < 0.0), param, "sigma_t(%lg) < 0",SIGMA_R); // modify condition to your needs
 
 	// insert your code here
-	return 0.0;
+	
+	Q = q;
+
+	if (SIGMA_R == 0.0) {
+		Pcs=homo_cyl_I_core(R,param);
+	} else {
+		find_LogNorm_int_range(2,R,SIGMA_R,&Rstart,&Rend,param);
+		Pcs 	= sasfit_integrate(Rstart, Rend, &homo_cyl_I_core, param);
+	}
+	return Pcs;
 }
 
 scalar sasfit_ff_pcs_homogeneouscyl_f(scalar q, sasfit_param * param)
 {
+	scalar Pcs, Rstart, Rend;
 	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
 
 	// insert your code here
-	return 0.0;
+	
+	Q = q;
+
+	if (SIGMA_R == 0.0) {
+		Pcs=homo_cyl_Ampl_core(R,param);
+	} else {
+		find_LogNorm_int_range(2,R,SIGMA_R,&Rstart,&Rend,param);
+		Pcs 	= sasfit_integrate(Rstart, Rend, &homo_cyl_Ampl_core, param);
+	}
+	return Pcs;
 }
 
 scalar sasfit_ff_pcs_homogeneouscyl_v(scalar q, sasfit_param * param, int dist)
