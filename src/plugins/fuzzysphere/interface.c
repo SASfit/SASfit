236436bbf5dd669697a9f397a9f60973f1ebcd50
Kohlbrecher <joachim.kohlbrecher@psi.ch>
2012-04-16 08:40:24 +0200

    - Extended the FuzzySphere plugin and moved LinShell and ExpShell from the old way of implementation into the plugin. - extended the documentation for spheres with fuzzy interfaces.

--- src/plugins/fuzzysphere/interface.c
+++ src/plugins/fuzzysphere/interface.c
@@ -1,18 +1,23 @@
 /*
  * Author(s) of this file:
- *   <your name> (<email address>)
+ *   Joachim Kohlbrecher (joachim.kohlbrecher@psi.ch)
  */
 
 #ifdef MAKE_SASFIT_PLUGIN
 #include "include/private.h"
 
 // functions to mark for export
-SASFIT_PLUGIN_EXP_BEGIN(4)
+SASFIT_PLUGIN_EXP_BEGIN(10)
 SASFIT_PLUGIN_EXP_ADD( ff_fuzzysphere )
 SASFIT_PLUGIN_EXP_ADD( ff_radial_profile_of_fuzzysphere )
 SASFIT_PLUGIN_EXP_ADD( ff_coreshellmicrogel )
 SASFIT_PLUGIN_EXP_ADD( ff_radial_profile_of_coreshellmicrogel )
-
+SASFIT_PLUGIN_EXP_ADD( ff_expshell )
+SASFIT_PLUGIN_EXP_ADD( ff_radial_profile_of_expshell )
+SASFIT_PLUGIN_EXP_ADD( ff_linshell )
+SASFIT_PLUGIN_EXP_ADD( ff_radial_profile_of_linshell )
+SASFIT_PLUGIN_EXP_ADD( ff_linshell2 )
+SASFIT_PLUGIN_EXP_ADD( ff_radial_profile_of_linshell2 )
 SASFIT_PLUGIN_EXP_END
 
 // import functions from other plugins
---
+++ src/plugins/fuzzysphere/sasfit_ff_radial_profile_of_LinShell2.c
@@ -0,0 +1,58 @@
+/*
+ * Author(s) of this file:
+ *   Joachim Kohlbrecher (joachim.kohlbrecher@psi.ch)
+ */
+
+#include "include/private.h"
+#include <sasfit_error_ff.h>
+
+// define shortcuts for local parameters/variables
+#define R_TOT		fabs(param->p[0])
+#define DR			fabs(param->p[1])
+#define ETA_CORE	param->p[2]
+#define ETA_SHELL	param->p[3]
+#define X_IN_SOL	param->p[4]
+#define X_OUT_SOL	param->p[5]
+#define ETA_SOL		param->p[6]
+#define R_CORE		((R_TOT-DR>=0) ? R_TOT-DR : 0)
+
+
+scalar sasfit_ff_radial_profile_of_linshell2(scalar r, sasfit_param * param)
+{
+	scalar m,b,eta_sh_in, eta_sh_out;
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	// insert your code here
+
+	eta_sh_in = (1- X_IN_SOL)*ETA_SHELL+ X_IN_SOL*ETA_SOL;
+	eta_sh_out= (1-X_OUT_SOL)*ETA_SHELL+X_OUT_SOL*ETA_SOL;
+	m=1e100;
+	if (DR !=0) {
+		m=(eta_sh_out-eta_sh_in)/DR;
+	}
+	b=-m*R_CORE+eta_sh_in;
+	if (fabs(r)<=R_CORE) {
+		return ETA_CORE;
+	} else if ((fabs(r)>R_CORE) && (fabs(r)<R_CORE+DR)) {
+		return m*fabs(r)+b;
+	} else {
+		return ETA_SOL;
+	}
+}
+
+scalar sasfit_ff_radial_profile_of_linshell2_f(scalar q, sasfit_param * param)
+{
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	// insert your code here
+	return 0.0;
+}
+
+scalar sasfit_ff_radial_profile_of_linshell2_v(scalar q, sasfit_param * param, int dist)
+{
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	// insert your code here
+	return 0.0;
+}
+
