fac02ea245c715bdb977a025aebaccc69810fae8
Ingo Bre?ler <dev@ingobressler.net>
2013-01-30 17:32:43 +0100

    saskit: added /usr/X11R6 to lib search paths, for MacOS 10.6

--- saskit/gen/common.sh
+++ saskit/gen/common.sh
@@ -7,7 +7,7 @@ fi
 SYSTEM_NAME=$(uname -s)
 SYSTEM_ARCH=$(uname -m)
 if [ "$SYSTEM_NAME" == "Darwin" ]; then
-	export CFLAGS="-arch $SYSTEM_ARCH -L/usr/X11/lib -I/usr/X11/include"
+	export CFLAGS="-arch $SYSTEM_ARCH -L/usr/X11/lib -L/usr/X11R6/lib -I/usr/X11/include"
 	export LDFLAGS=$CFLAGS
 	export CPPFLAGS=$CFLAGS
 fi;
