a8672093eeecddd7e7f95b78606c2ec081911b72
Ingo Breßler <dev@ingobressler.net>
2013-12-03 19:47:27 +0100

    embedded build of GSL, TCL & FFTW and saskit if not found

--- src/cmake/create_starpack.cmake
+++ src/cmake/create_starpack.cmake
@@ -42,27 +42,29 @@ set(SASFIT_EXEC ${SASFIT_ROOT_DIR}/${SASFIT_EXEC_FILENAME})
 
 message(STATUS "Creating Starpack ...")
 
-if(EXISTS ${SASKIT_FILE})
-	message(STATUS "Using ${SASKIT_FILE}")
-	if(NOT WIN32)
-		execute_process(COMMAND chmod u+x,g+x,o+x ${SASKIT_FILE})
-	endif(NOT WIN32)
-	file(REMOVE ${SASFIT_EXEC})
-	configure_file(${SASKIT_FILE} ${SASKIT_FILE}_rt COPYONLY)
-	execute_process(COMMAND ${SASKIT_FILE} "${SASKIT_PATH}/sdx.kit" wrap ${SASFIT_EXEC_FILENAME} -runtime ${SASKIT_FILE}_rt
-			WORKING_DIRECTORY ${SASFIT_ROOT_DIR}
-			TIMEOUT 80.0
-			RESULT_VARIABLE SASKIT_RES
-			OUTPUT_VARIABLE SASKIT_OUT
-			ERROR_VARIABLE SASKIT_OUT
-	)
-	if(SASKIT_RES EQUAL 0 OR SASKIT_RES STREQUAL "Process terminated due to timeout")
-		message(STATUS "Successfully created ${SASFIT_EXEC}")
-	else(SASKIT_RES EQUAL 0 OR SASKIT_RES STREQUAL "Process terminated due to timeout")
-		message(STATUS "An error occured while creating ${SASFIT_EXEC}!")
-		message("return value: '${SASKIT_RES}'")
-		message("output: '${SASKIT_OUT}'")
-	endif(SASKIT_RES EQUAL 0 OR SASKIT_RES STREQUAL "Process terminated due to timeout")
-	file(REMOVE ${SASKIT_FILE}_rt)
-endif(EXISTS ${SASKIT_FILE})
+if(NOT EXISTS "${SASKIT_FILE}")
+        return()
+endif()
+message(STATUS "Using ${SASKIT_FILE}")
+if(NOT WIN32)
+        execute_process(COMMAND chmod u+x,g+x,o+x ${SASKIT_FILE})
+endif(NOT WIN32)
+file(REMOVE ${SASFIT_EXEC})
+configure_file(${SASKIT_FILE} ${SASKIT_FILE}_rt COPYONLY)
+execute_process(COMMAND ${SASKIT_FILE} "${SASKIT_PATH}/sdx.kit" wrap ${SASFIT_EXEC_FILENAME} -runtime ${SASKIT_FILE}_rt
+                WORKING_DIRECTORY ${SASFIT_ROOT_DIR}
+                TIMEOUT 80.0
+                RESULT_VARIABLE SASKIT_RES
+                OUTPUT_VARIABLE SASKIT_OUT
+                ERROR_VARIABLE SASKIT_OUT
+)
+if(SASKIT_RES EQUAL 0 OR SASKIT_RES STREQUAL "Process terminated due to timeout")
+        message(STATUS "Successfully created ${SASFIT_EXEC}")
+else(SASKIT_RES EQUAL 0 OR SASKIT_RES STREQUAL "Process terminated due to timeout")
+        message(STATUS "An error occured while creating ${SASFIT_EXEC}!")
+        message("return value: '${SASKIT_RES}'")
+        message("output: '${SASKIT_OUT}'")
+endif(SASKIT_RES EQUAL 0 OR SASKIT_RES STREQUAL "Process terminated due to timeout")
+file(REMOVE ${SASKIT_FILE}_rt)
 
+# vim: set ts=4 sw=4 sts=4 tw=0:
