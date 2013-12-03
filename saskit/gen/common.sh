fea8c7376e194423b2cbe9cb16e60dda6b667e1c
Ingo Bre?ler <dev@ingobressler.net>
2013-12-03 19:42:34 +0100

    saskit fixed bash script bug

--- saskit/gen/common.sh
+++ saskit/gen/common.sh
@@ -4,9 +4,10 @@ if [ "x$TCLSH" = "x" ]; then
     TCLSH=tclsh
 fi
 
-SYSTEM_NAME=$(uname -s)
-SYSTEM_ARCH=$(uname -m)
-if [ "$SYSTEM_NAME" == "Darwin" ]; then
+SYSTEM_NAME="$(uname -s)";
+SYSTEM_ARCH="$(uname -m)";
+
+if [ "$SYSTEM_NAME" = "Darwin" ]; then
 	SYSTEM_ARCH="x86_64"
 	export CFLAGS="-arch $SYSTEM_ARCH -L/usr/X11/lib -L/usr/X11R6/lib -I/usr/X11/include"
 fi;
