63a21ebe061dec88501ac6cce9107c9893c96576
Ingo Breßler <dev@ingobressler.net>
2015-05-06 14:53:34 +0200

    fixed location retrieval of uname command in sasfit.sh startup wrapper

--- sasfit.sh
+++ sasfit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # http://stackoverflow.com/a/630387
 PATH="`dirname \"$0\"`"
-OS=$(/usr/bin/uname -s)
+UNAME="/bin/uname"
+[ -f "$UNAME" ] || UNAME="/usr/bin/uname"
+OS=$($UNAME -s)
 if [ "$OS" == "Darwin" ]; then
   DYLD_LIBRARY_PATH="$PATH:$DYLD_LIBRARY_PATH" "$PATH"/sasfit
 else
