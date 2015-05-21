099c90023975c76d9d1d223f6aa5f23c091dd1a5
Ingo Breßler <dev@ingobressler.net>
2015-05-21 15:37:39 +0200

    fixed erroneous PATH var overwrite in startup wrapper script

--- sasfit.sh
+++ sasfit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # http://stackoverflow.com/a/630387
-PATH="`dirname \"$0\"`"
+DIR="`dirname \"$0\"`"
 UNAME="/bin/uname"
 [ -f "$UNAME" ] || UNAME="/usr/bin/uname"
 OS=$($UNAME -s)
 if [ "$OS" == "Darwin" ]; then
-  DYLD_LIBRARY_PATH="$PATH:$DYLD_LIBRARY_PATH" "$PATH"/sasfit
+  DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH" "$DIR"/sasfit
 else
-  LIBPATH="$PATH/lib"
-  "$LIBPATH"/ld-[0-9].[0-9][0-9].so --library-path "$LIBPATH" "$PATH"/sasfit
+  LIBPATH="$DIR/lib"
+  "$LIBPATH"/ld-[0-9].[0-9][0-9].so --library-path "$LIBPATH" "$DIR"/sasfit
 fi;
