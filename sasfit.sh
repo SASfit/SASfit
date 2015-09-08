d57953f61f07ebdad5658a365f7b451c8f5eccae
Ingo Breßler <dev@ingobressler.net>
2015-09-08 15:06:28 +0200

    including OpenMP dependencies, tested on Windows

--- sasfit.sh
+++ sasfit.sh
@@ -6,7 +6,9 @@ UNAME="/bin/uname"
 OS=$($UNAME -s)
 if [ "$OS" == "Darwin" ]; then
   DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH" "$DIR"/sasfit
-else
+elif [ -f "$LIBPATH"/ld-[0-9].[0-9][0-9].so ]; then
   LIBPATH="$DIR/lib"
   "$LIBPATH"/ld-[0-9].[0-9][0-9].so --library-path "$LIBPATH" "$DIR"/sasfit
 fi;
+
+# vim: set ts=2 sts=2 sw=2 tw=0:
