659d38510764ab04f0e1baa308699ccc41751f01
Ingo Bressler <dev@ingobressler.net>
2016-07-10 16:23:54 +0200

    fixed startup shell script

--- sasfit.sh
+++ sasfit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # http://stackoverflow.com/a/630387
 DIR="`dirname \"$0\"`"
+LIBPATH="lib"
 UNAME="/bin/uname"
 [ -f "$UNAME" ] || UNAME="/usr/bin/uname"
 OS=$($UNAME -s)
 if [ "$OS" == "Darwin" ]; then
   DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH" "$DIR"/sasfit
 elif [ -f "$LIBPATH"/ld-[0-9].[0-9][0-9].so ]; then
-  LIBPATH="$DIR/lib"
+  LIBPATH="$DIR/$LIBPATH"
   "$LIBPATH"/ld-[0-9].[0-9][0-9].so --library-path "$LIBPATH" "$DIR"/sasfit
 fi;
 
