cba70bed3c568c2eaa37a1d47d13cc8cab089610
kohlbrecher <joachim.kohlbrecher@psi.ch>
2018-06-01 15:57:54 +0200

    new plugin structure factors for 2-dimensional fluids of of monodisperse hard spheres, "2D hard disks (Rosenfeld)" and "2D hard disks (Guo)"

---
+++ src/plugins/two_dim_fluid/include/sasfit_two_dim_fluid.h
@@ -0,0 +1,125 @@
+/*
+ * Author(s) of this file:
+ *   <your name> (<email address>)
+ */
+
+#ifndef SASFIT_PLUGIN_TWO_DIM_FLUID_H
+#define SASFIT_PLUGIN_TWO_DIM_FLUID_H
+
+#include <sasfit_common_shared_exports.h>
+
+/**
+ * \file sasfit_two_dim_fluid.h
+ * Public available functions and descriptions of the two_dim_fluid plugin.
+ */
+
+/**
+ * \def sasfit_two_dim_fluid_DLLEXP
+ * \copydoc sasfit_common_DLLEXP
+ */
+
+// adjust the project name below
+// *_EXPORTS is set by cmake if build as shared library
+#if defined(sasfit_two_dim_fluid_EXPORTS)
+	#ifdef sasfit_two_dim_fluid_DLLEXP
+	#undef sasfit_two_dim_fluid_DLLEXP
+	#endif
+	#define sasfit_two_dim_fluid_DLLEXP SASFIT_LIB_EXPORT
+#elif !defined(sasfit_two_dim_fluid_DLLEXP)
+	// is set somewhere else for export as non-plugin
+	#define sasfit_two_dim_fluid_DLLEXP SASFIT_LIB_IMPORT
+#endif
+
+// general information about the form factor here
+// & info used in the GUI:
+// - group definition & uplevel group this one is in
+// - brief description
+// - description of parameters in HTML table-style
+
+/* ################ start sq__d_hard_disks__rosenfeld_ ################ */
+/** 
+ * \defgroup sq__d_hard_disks__rosenfeld_ 2D hard disks (Rosenfeld)
+ * \ingroup sq_plugins_devel
+ *
+ * \brief \<some brief description of 2D hard disks (Rosenfeld) function\>
+ *
+ * <more detailed documentation, see 'doxygen' docs>
+ *
+ *
+ * \par Required parameters:
+ *      <table border="0"><tr>
+ *       <td>\b R</td>
+ *       <td>radius of the discs</td>
+ *      </tr><tr>
+ *       <td>\b eta</td>
+ *       <td>surface coverage</td>
+ *      </tr></table>
+ */
+
+/**
+ * \ingroup sq__d_hard_disks__rosenfeld_
+ *
+ * \sa sasfit_two_dim_fluid.h, sq_plugins_devel
+ */
+sasfit_two_dim_fluid_DLLEXP scalar sasfit_sq__d_hard_disks__rosenfeld_(scalar q, sasfit_param * p);
+
+/**
+ * \ingroup sq__d_hard_disks__rosenfeld_
+ *
+ * \sa sasfit_two_dim_fluid.h, sq_plugins_devel
+ */
+sasfit_two_dim_fluid_DLLEXP scalar sasfit_sq__d_hard_disks__rosenfeld__f(scalar q, sasfit_param * p);
+
+/**
+ * \ingroup sq__d_hard_disks__rosenfeld_
+ *
+ * \sa sasfit_two_dim_fluid.h, sq_plugins_devel
+ */
+sasfit_two_dim_fluid_DLLEXP scalar sasfit_sq__d_hard_disks__rosenfeld__v(scalar q, sasfit_param * p, int dist);
+/* ################ stop sq__d_hard_disks__rosenfeld_ ################ */
+
+/* ################ start sq__d_hard_disks__guo_ ################ */
+/** 
+ * \defgroup sq__d_hard_disks__guo_ 2D hard disks (Guo)
+ * \ingroup sq_plugins_devel
+ *
+ * \brief \<some brief description of 2D hard disks (Guo) function\>
+ *
+ * <more detailed documentation, see 'doxygen' docs>
+ *
+ *
+ * \par Required parameters:
+ *      <table border="0"><tr>
+ *       <td>\b R</td>
+ *       <td>radius of hard disks</td>
+ *      </tr><tr>
+ *       <td>\b eta</td>
+ *       <td>surface coverage</td>
+ *      </tr></table>
+ */
+
+/**
+ * \ingroup sq__d_hard_disks__guo_
+ *
+ * \sa sasfit_two_dim_fluid.h, sq_plugins_devel
+ */
+sasfit_two_dim_fluid_DLLEXP scalar sasfit_sq__d_hard_disks__guo_(scalar q, sasfit_param * p);
+
+/**
+ * \ingroup sq__d_hard_disks__guo_
+ *
+ * \sa sasfit_two_dim_fluid.h, sq_plugins_devel
+ */
+sasfit_two_dim_fluid_DLLEXP scalar sasfit_sq__d_hard_disks__guo__f(scalar q, sasfit_param * p);
+
+/**
+ * \ingroup sq__d_hard_disks__guo_
+ *
+ * \sa sasfit_two_dim_fluid.h, sq_plugins_devel
+ */
+sasfit_two_dim_fluid_DLLEXP scalar sasfit_sq__d_hard_disks__guo__v(scalar q, sasfit_param * p, int dist);
+/* ################ stop sq__d_hard_disks__guo_ ################ */
+
+
+#endif // this file
+
---
+++ src/plugins/two_dim_fluid/sasfit_sq__d_hard_disks__rosenfeld_.c
@@ -0,0 +1,50 @@
+/*
+ * Author(s) of this file:
+ *   <your name> (<email address>)
+ */
+
+#include "include/private.h"
+#include <sasfit_error_sq.h>
+
+// define shortcuts for local parameters/variables
+#define R	param->p[0]
+#define ETA	param->p[1]
+
+scalar sasfit_sq__d_hard_disks__rosenfeld_(scalar q, sasfit_param * param)
+{
+    scalar A, B, Z, XI, G, _1Sq, qR;
+
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	SASFIT_CHECK_COND1((q < 0.0), param, "q(%lg) < 0",q);
+	SASFIT_CHECK_COND1((R < 0.0), param, "R(%lg) < 0",R); // modify condition to your needs
+	SASFIT_CHECK_COND1((ETA <= 0.0), param, "eta(%lg) <= 0",ETA); // modify condition to your needs
+	SASFIT_CHECK_COND1((ETA >= 1.0), param, "eta(%lg) >= 1",ETA); // modify condition to your needs
+	// insert your code here
+
+    G = pow(1-ETA,-1.5);
+    XI = (1.+ETA)/gsl_pow_3(1.-ETA);
+    B=((1.-ETA)*XI-1.-3.*ETA*G)/ETA;
+	A=(1.+(2.*ETA-1.)*XI+2*ETA*G)/ETA;
+	_1Sq= 1.+4.*ETA*(A*gsl_pow_2(gsl_sf_bessel_J1(qR)/(qR))
+                    +B*gsl_sf_bessel_J0(qR)*gsl_sf_bessel_J1(qR)/qR
+                    +G*gsl_sf_bessel_J1(2.*qR)/qR);
+	return 1./_1Sq;
+}
+
+scalar sasfit_sq__d_hard_disks__rosenfeld__f(scalar q, sasfit_param * param)
+{
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	// insert your code here
+	return 0.0;
+}
+
+scalar sasfit_sq__d_hard_disks__rosenfeld__v(scalar q, sasfit_param * param, int dist)
+{
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	// insert your code here
+	return 0.0;
+}
+
