225fa53bbe88c366916468484fec07eafa1a1580
Ingo Bressler <dev@ingobressler.net>
2019-03-04 11:47:15 +0100

    cmake: check for any patches before trying to apply them

--- src/cmake/SasfitCmakeUtils.cmake
+++ src/cmake/SasfitCmakeUtils.cmake
@@ -404,7 +404,7 @@ function(build_from_source CURRENT_DIR CONFIG_OPTIONS)
     get_filename_component(WORK_DIR "${WORK_DIR}" REALPATH)
 
     # applying any patches lying around in ${CURRENT_DIR}
-    execute_process(COMMAND sh -c "for fn in $(ls -1 ../*.patch); do
+    execute_process(COMMAND sh -c "ls ../*.patch && for fn in $(ls -1 ../*.patch); do
                                      echo \"Applying '$fn':\";
                                      patch -p1 < \"$fn\";
                                    done"
