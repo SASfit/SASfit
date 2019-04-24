9ab09498c81900d1358b86adbb16713a9d8486aa
Ingo Bressler <dev@ingobressler.net>
2019-04-24 17:08:08 +0200

    appveyor: list directory contents after build for debugging

--- src/appveyor/build.sh
+++ src/appveyor/build.sh
@@ -3,4 +3,9 @@
 cd $APPVEYOR_BUILD_FOLDER
 mkdir build && cd build && cmake ../src && make
 
+for dir in "$APPVEYOR_BUILD_FOLDER/.." "$APPVEYOR_BUILD_FOLDER" "$APPVEYOR_BUILD_FOLDER/src"; do
+  echo "$dir:"
+  ls -la "$dir"
+done
+
 # vim: set ts=2 sw=2 sts=2 tw=0:
