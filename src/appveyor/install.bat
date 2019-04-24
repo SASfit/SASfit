81e7c9351a60462367d7995c20ebdd1821047895
Ingo Bressler <dev@ingobressler.net>
2019-04-24 17:24:24 +0200

    appveyor: fixed echo of newline

--- src/appveyor/install.bat
+++ src/appveyor/install.bat
@@ -11,21 +11,22 @@ echo Bits: %BIT%
 
 IF NOT %COMPILER%==msys2 exit
 
-echo ## Updating PATH for MSYS2/MinGW64:
+@echo.
+@echo ## Updating PATH for MSYS2/MinGW64:
 @echo on
 set "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 
-@echo
+@echo.
 @echo ## Ensure pacman mirrors and GIT are up to date:
 sh -lc "pacman -S --needed --noconfirm pacman-mirrors"
 sh -lc "pacman -S --needed --noconfirm git"
 
-@echo
+@echo.
 @echo ## Updating packages first:
 sh -lc "pacman -Syu --noconfirm"
 sh -lc "pacman -Syu --noconfirm"
 
-@echo
+@echo.
 @echo ## Installing required packages:
 sh -lc "pacman -S --noconfirm cmake make diffutils patch"
 
