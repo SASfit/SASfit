a0e2b2d5f7571b74ed9d1e24c551860750fb5546
Ingo Bressler <dev@ingobressler.net>
2019-04-24 15:30:41 +0200

    appveyor: updated install script for readability

--- src/appveyor/install.bat
+++ src/appveyor/install.bat
@@ -11,18 +11,22 @@ echo Bits: %BIT%
 
 IF NOT %COMPILER%==msys2 exit
 
+echo ## Updating PATH for MSYS2/MinGW64:
 @echo on
 set "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 
-@echo "Ensure pacman mirrors and GIT are up to date:"
+@echo
+@echo ## Ensure pacman mirrors and GIT are up to date:
 sh -lc "pacman -S --needed --noconfirm pacman-mirrors"
 sh -lc "pacman -S --needed --noconfirm git"
 
-@echo "Updating packages first:"
+@echo
+@echo ## Updating packages first:
 sh -lc "pacman -Syu --noconfirm"
 sh -lc "pacman -Syu --noconfirm"
 
-@echo "Installing required packages:"
+@echo
+@echo ## Installing required packages:
 sh -lc "pacman -S --noconfirm cmake make diffutils patch"
 
 @echo off
