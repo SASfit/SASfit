56dff35f18816cd0b1666aa5871954c3e8a96d41
Ingo Breßler <ingo.bressler@bam.de>
2019-04-26 12:00:47 +0200

    appveyor: output Doxyfile for debugging

--- src/appveyor/build.sh
+++ src/appveyor/build.sh
@@ -7,5 +7,6 @@ for dir in "$APPVEYOR_BUILD_FOLDER/.." "$APPVEYOR_BUILD_FOLDER" "$APPVEYOR_BUILD
   echo "$dir:"
   ls -la "$dir"
 done
+head -n 40 "$APPVEYOR_BUILD_FOLDER/src/Doxyfile"
 
 # vim: set ts=2 sw=2 sts=2 tw=0:
