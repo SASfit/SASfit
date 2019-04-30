2ff9bf321fc9498fbf1428d0030d6b4a7fe3b791
Ingo Bressler <dev@ingobressler.net>
2019-04-30 12:30:30 +0200

    appveyor: restructured build config

---
+++ src/appveyor/before_build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo
+echo "## Listing environment variables ..."
+echo
+env | sort
+
+# vim: set ts=2 sw=2 sts=2 tw=0:
--- src/appveyor/build.sh
+++ src/appveyor/build.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
 echo
-echo "## Listing environment variables ..."
-echo
-env | sort
-echo
 echo "## Starting build ..."
 echo
 
-cd $APPVEYOR_BUILD_FOLDER
-mkdir build && cd build && cmake ../src && make
-
-for dir in "$APPVEYOR_BUILD_FOLDER/.." "$APPVEYOR_BUILD_FOLDER" "$APPVEYOR_BUILD_FOLDER/src"; do
-  echo "$dir:"
-  ls -la "$dir"
-done
-head -n 40 "$APPVEYOR_BUILD_FOLDER/src/Doxyfile"
+cd "$APPVEYOR_BUILD_FOLDER" && \
+mkdir build && cd build && cmake ../src
 
 # vim: set ts=2 sw=2 sts=2 tw=0:
