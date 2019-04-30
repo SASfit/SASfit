d28d5fbc9928d2e3e5f6adba02a3653be654bfa8
Ingo Bressler <dev@ingobressler.net>
2019-04-30 14:08:16 +0200

    appveyor: build script sets custom build version and finds number of cpus

--- src/appveyor/build.sh
+++ src/appveyor/build.sh
@@ -4,7 +4,15 @@ echo
 echo "## Starting build ..."
 echo
 
+eval "$(wmic cpu get /format:list | grep NumberOfLogicalProcessors)"
+
 cd "$APPVEYOR_BUILD_FOLDER" && \
 mkdir build && cd build && cmake ../src
+echo
+echo "## Setting AppVeyor build version to '$SASFIT_VERSION'"
+echo
+appveyor UpdateBuild -Version "$SASFIT_VERSION"
+echo "NumberOfLogicalProcessors: $NumberOfLogicalProcessors"
+make -j$NumberOfLogicalProcessors
 
 # vim: set ts=2 sw=2 sts=2 tw=0:
