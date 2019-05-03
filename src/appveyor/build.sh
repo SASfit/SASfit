1122dbd9e7cd4796653d787425a890220315f549
Ingo Breßler <ingo.bressler@bam.de>
2019-05-03 12:10:08 +0200

    appveyor: updated determination of number of computing cores to fix build error

--- src/appveyor/build.sh
+++ src/appveyor/build.sh
@@ -8,12 +8,15 @@ NUM_LOGICAL_CORES=1
 # determine number of computing cores on Windows
 WMIC="$(command -v wmic)"
 if [ -x "$WMIC" ]; then
-    NUM_LOGICAL_CORES="$($WMIC cpu get /format:list | grep NumberOfLogicalProcessors | cut -d= -f2)"
+    NUM_LOGICAL_CORES="$($WMIC cpu get /format:list \
+        | grep NumberOfLogicalProcessors \
+        | cut -d= -f2 \
+        | tr -d '[:space:]' )"
 fi
 export NUM_LOGICAL_CORES
 echo "$0 determined $NUM_LOGICAL_CORES logical cores."
 
 cd "$APPVEYOR_BUILD_FOLDER" && \
-mkdir build && cd build && cmake ../src && make -j"$NUM_LOGICAL_CORES"
+mkdir build && cd build && cmake ../src && make -j$NUM_LOGICAL_CORES
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
