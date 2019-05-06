8c9c9a1d28cf4c0e2401fde1f599c4f417b0a5da
Ingo Bressler <dev@ingobressler.net>
2019-05-06 16:06:52 +0200

    appveyor: fix after_deploy script for added package file to download list

--- src/appveyor/after_deploy.sh
+++ src/appveyor/after_deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # some bintray tweaks after deployment
 
-#echo
-#echo "Environment variables:"
-#echo
-#env | sort
+echo "Find package:"
+find "$APPVEYOR_BUILD_FOLDER/src" -type f -name "*$APPVEYOR_BUILD_VERSION*"
 
+echo "Call bintray api:"
 FN="$(find "$APPVEYOR_BUILD_FOLDER/src" -type f \
-    -name "*$APPVEYOR_BUILD_VERSION*" -printf '%f')"
+    -name "*$APPVEYOR_BUILD_VERSION*" -printf '%f\n' | head -n1)"
 curl -usasfit:$BT_API -H Content-Type:application/json -H Accept:application/json \
      -X PUT -d '{ "list_in_downloads":true }' \
-     https://api.bintray.com/file_metadata/sasfit/development/$FN
+     "https://api.bintray.com/file_metadata/sasfit/development/$FN"
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
