bf1b9807033d7ba125f15812d539c6cb8a5c064f
Ingo Bressler <dev@ingobressler.net>
2019-05-06 17:54:12 +0200

    appveyor: Wait for uploaded file being published before adding it to download list

--- src/appveyor/after_deploy.sh
+++ src/appveyor/after_deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 # some bintray tweaks after deployment
 
-echo "Find package:"
-find "$APPVEYOR_BUILD_FOLDER/src" -type f -name "*$APPVEYOR_BUILD_VERSION*"
+#echo "Find package:"
+#find "$APPVEYOR_BUILD_FOLDER/src" -type f -name "*$APPVEYOR_BUILD_VERSION*"
 
-echo "Call bintray api:"
-FN="$(find "$APPVEYOR_BUILD_FOLDER/src" -type f \
+echo "Wait for uploaded file being published?"
+sleep 30
+# publish first?
+# POST /content/:subject/:repo/:package/:version/publish
+# https://bintray.com/docs/api/
+#curl -s -usasfit:$BT_API -H Content-Type:application/json -H Accept:application/json \
+#    -d '{ "publish_wait_for_secs": -1 }' \
+#    -X POST "https://api.bintray.com/content/sasfit/development/SASfit/$APPVEYOR_BUILD_VERSION/publish"
+
+echo "Add uploaded file to download list:"
+FN="$(find "$APPVEYOR_BUILD_FOLDER/src" -maxdepth 1 -type f \
     -name "*$APPVEYOR_BUILD_VERSION*" -printf '%f\n' | head -n1)"
-curl -usasfit:$BT_API -H Content-Type:application/json -H Accept:application/json \
+curl -s -usasfit:$BT_API -H Content-Type:application/json -H Accept:application/json \
      -X PUT -d '{ "list_in_downloads":true }' \
      "https://api.bintray.com/file_metadata/sasfit/development/$FN"
 
