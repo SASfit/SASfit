c8a7cbcd94595c2781d4a5355b277e1f7bc29352
Ingo Breßler <dev@ingobressler.net>
2020-03-23 09:50:00 -0700

    updated startup script for macOS
      - needed to load libstc++ from the package which is more recent than the one on the system by default

--- sasfit.sh
+++ sasfit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# http://stackoverflow.com/a/630387
-DIR="`dirname \"$0\"`"
-LIBPATH="lib"
+# start SASfit and point it to the shipped dynamic libraries
+SCRIPTDIR="`dirname \"$0\"`"
+LIBDIR="$SCRIPTDIR/lib"
 UNAME="/bin/uname"
 [ -f "$UNAME" ] || UNAME="/usr/bin/uname"
-OS=$($UNAME -s)
+OS="$($UNAME -s)"
 if [ "$OS" == "Darwin" ]; then
-  DYLD_LIBRARY_PATH="$DIR:$DYLD_LIBRARY_PATH" "$DIR"/sasfit
-elif [ -f "$LIBPATH"/ld-[0-9].[0-9][0-9].so ]; then
-  LIBPATH="$DIR/$LIBPATH"
-  "$LIBPATH"/ld-[0-9].[0-9][0-9].so --library-path "$LIBPATH" "$DIR"/sasfit
+  DYLD_LIBRARY_PATH="$LIBDIR:$DYLD_LIBRARY_PATH" "$SCRIPTDIR"/sasfit
+elif [ -f "$LIBDIR"/ld-[0-9].[0-9][0-9].so ]; then
+  # use own library loader on Linux
+  "$LIBDIR"/ld-[0-9].[0-9][0-9].so --library-path "$LIBDIR" "$SCRIPTDIR"/sasfit
 fi;
 
 # vim: set ts=2 sts=2 sw=2 tw=0:
