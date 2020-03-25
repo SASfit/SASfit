b9b859191ee7e171fdb5a09422f7e0dfa46cd186
Ingo Breßler <dev@ingobressler.net>
2020-03-25 13:40:53 +0100

    appveyor: list contents of binary package after build

--- src/appveyor/after_build.sh
+++ src/appveyor/after_build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # some info for debugging after build
 
-for dir in "$APPVEYOR_BUILD_FOLDER/.." "$APPVEYOR_BUILD_FOLDER" "$APPVEYOR_BUILD_FOLDER/src"; do
+for dir in "$APPVEYOR_BUILD_FOLDER/.." \
+           "$APPVEYOR_BUILD_FOLDER" \
+           "$APPVEYOR_BUILD_FOLDER/src" \
+           "$APPVEYOR_BUILD_FOLDER/../sasfit_*"; do
     echo "$dir:"
     ls -la "$dir"
 done
