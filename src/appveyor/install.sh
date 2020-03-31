5dc32176803b9c4e46cf225bc783bf49023aa5bc
Ingo Breßler <dev@ingobressler.net>
2020-03-31 15:49:45 +0200

    appveyor: using shell script for install stage
    - instead of batch script

---
+++ src/appveyor/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo
+echo "## Installing build tools ..."
+echo
+
+env
+
+if [ "$COMPILER" = msys2 ];
+then
+    export PATH="/$MSYSTEM/bin:/usr/bin:$PATH"
+
+    # Ensure pacman mirrors and GIT are up to date:
+    pacman -S --needed --noconfirm pacman-mirrors
+    pacman -S --needed --noconfirm git
+
+    # Updating packages first:
+    pacman -Syu --noconfirm
+    pacman -Syu --noconfirm
+
+    # Installing required packages:
+    pacman -S --noconfirm make diffutils patch mingw-w64-x86_64-gcc mingw-w64-x86_64-cmake
+fi
+
+# vim: set ts=4 sw=4 sts=4 tw=0 et:
