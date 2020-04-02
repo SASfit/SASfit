93242aac32d538c6d6d61cb81a36af3cac57c9b7
Ingo Breßler <dev@ingobressler.net>
2020-04-02 14:22:18 +0200

    appveyor: testing macos environment & brew

--- src/appveyor/0_install_macos.sh
+++ src/appveyor/0_install_macos.sh
@@ -6,17 +6,20 @@ echo
 
 env
 
-#if [ "$COMPILER" = msys2 ];
-#then # on Windows
-    set -x
-#    export PATH="/c/$MSYS2_DIR/$MSYSTEM/bin:/c/$MSYS2_DIR/opt/bin:/c/$MSYS2_DIR/usr/local/bin:/c/$MSYS2_DIR/usr/bin:/c/$MSYS2_DIR/bin:$PATH"
-#    ROOT_DIR=/c/$MSYS2_DIR
-#    export PATH="$ROOT_DIR/$MSYSTEM/bin:$ROOT_DIR/usr/bin:$ROOT_DIR/bin"
-    echo "CMD: '$0' argv: '$@'"
-    which brew
-    brew -h
-    brew install git cmake gcc
-    brew cask install xquartz
-#fi
+set -x
+echo "CMD: '$0' argv: '$@'"
+uname -s
+which cmake
+cmake --version
+which gcc
+echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^gcc'
+
+which brew
+brew -h
+brew update
+brew cleanup
+# already installed: git, cmake
+#brew install cmake gcc
+brew cask install xquartz
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
