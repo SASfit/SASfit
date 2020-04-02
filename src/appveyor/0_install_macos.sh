79c0acb1deaa640db21bb1da0bd33222f23829ac
Ingo Breßler <dev@ingobressler.net>
2020-04-02 13:27:19 +0200

    appveyor: testing macos environment & brew

--- src/appveyor/0_install_macos.sh
+++ src/appveyor/0_install_macos.sh
@@ -13,27 +13,10 @@ env
 #    ROOT_DIR=/c/$MSYS2_DIR
 #    export PATH="$ROOT_DIR/$MSYSTEM/bin:$ROOT_DIR/usr/bin:$ROOT_DIR/bin"
     echo "CMD: '$0' argv: '$@'"
-    pid=$$
-    /bin/ps -p $pid
-    echo "test"
-    ps -p $pid
-    which ps
-    which sh
     which brew
     brew -h
-exit 0
-    sh -lc "pacman"
-
-    # Ensure pacman mirrors and GIT are up to date:
-    pacman -S --needed --noconfirm pacman-mirrors
-    pacman -S --needed --noconfirm git
-
-    # Updating packages first:
-    pacman -Syu --noconfirm
-    pacman -Syu --noconfirm
-
-    # Installing required packages:
-    pacman -S --noconfirm make diffutils patch mingw-w64-x86_64-gcc mingw-w64-x86_64-cmake
+    brew install git cmake gcc
+    brew cask install xquartz
 #fi
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
