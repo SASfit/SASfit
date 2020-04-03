291a8c56756462e2d21c091bf1c61b5d4d752233
Ingo Breßler <dev@ingobressler.net>
2020-04-03 11:48:43 +0200

    appveyor: determine num of logical cores on macOS

--- src/appveyor/4_build.sh
+++ src/appveyor/4_build.sh
@@ -3,28 +3,29 @@
 echo
 echo "## Starting build ..."
 echo
+echo "uname -s: '$(uname -s)'"
 
 NUM_LOGICAL_CORES=1
-# determine number of computing cores on Windows
-WMIC="$(command -v wmic)"
-if [ -x "$WMIC" ]; then
-    NUM_LOGICAL_CORES="$($WMIC cpu get /format:list \
-        | grep NumberOfLogicalProcessors \
-        | cut -d= -f2 \
-        | tr -d '[:space:]' )"
-fi
-export NUM_LOGICAL_CORES
-echo "$0 determined $NUM_LOGICAL_CORES logical cores."
-
-echo "uname -s: '$(uname -s)'"
 CMAKE_GENERATOR='Unix Makefiles'
-#if <Windows>; then CMAKE_GENERATOR='MSYS Makefiles'; fi
-if [ "$(uname -s)" = "Darwin" ];
+if [ "$(uname -s)" = "Darwin" ]; # macOS
 then
+    # determine number of computing cores
+    NUM_LOGICAL_CORES=$(sysctl -n hw.ncpu)
     # find latest gcc and g++ compilers and set them as global variables
     export CC=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^gcc-(mp-)?[0-9]+' | tail -n1))
     export CXX=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^g\+\+-(mp-)?[0-9]+' | tail -n1))
+else # Windows
+    WMIC="$(command -v wmic)"
+    if [ -x "$WMIC" ]; then
+        NUM_LOGICAL_CORES="$($WMIC cpu get /format:list \
+            | grep NumberOfLogicalProcessors \
+            | cut -d= -f2 \
+            | tr -d '[:space:]' )"
+    fi
+    CMAKE_GENERATOR='MSYS Makefiles'
 fi
+export NUM_LOGICAL_CORES
+echo "$0 determined $NUM_LOGICAL_CORES logical cores."
 
 cd "$APPVEYOR_BUILD_FOLDER" && \
 mkdir build && cd build && cmake -G "$CMAKE_GENERATOR" ../src && make -j$NUM_LOGICAL_CORES
