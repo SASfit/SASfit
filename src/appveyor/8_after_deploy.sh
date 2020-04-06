145bcb005f031477a0e5ada41ba53806a378da9d
Ingo Breßler <dev@ingobressler.net>
2020-04-06 18:54:17 +0200

    appveyor: fixed after_deploy script for listing files as direct downloads at BinTray

--- src/appveyor/8_after_deploy.sh
+++ src/appveyor/8_after_deploy.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
-# some bintray tweaks after deployment
+# Using the BinTray REST API for some tweaks after deployment.
+# Make sure all uploaded files appear in the direct downloads section
 
-CURL="curl -s -usasfit:$BT_API -H Content-Type:application/json -H Accept:application/json"
+CURL="curl -s -u sasfit:$BT_API -H Content-Type:application/json -H Accept:application/json"
+API_URL="https://api.bintray.com"
+PKG_PATH="sasfit/development/SASfit"
 
-echo "Wait for uploaded file being published:"
-#sleep 30
-# publish first?
-# POST /content/:subject/:repo/:package/:version/publish
-# https://bintray.com/docs/api/
-out=$($CURL -X POST -d '{ "publish_wait_for_secs": -1 }' \
-      "https://api.bintray.com/content/sasfit/development/SASfit/$SASFIT_VERSION/publish")
-echo "    $out"
-# stop here if the artifact was not uploaded
-(echo "$out" | grep -q 'not found') && exit 1
+# get the latest version, previously created one, probably
+latest_ver=$($CURL -X GET "$API_URL/packages/$PKG_PATH" \
+    | grep -oE 'latest_version"?:"?[^"]+' | grep -oE '[^"]+$')
+echo "Latest version on BinTray: '$latest_ver'."
 
-echo "Add uploaded file to download list:"
-FN="$(find "$APPVEYOR_BUILD_FOLDER/src" -maxdepth 1 -type f \
-    -name "*$SASFIT_VERSION*" | head -n1)"
-FN="${FN##*/}"
-echo "FN: '$FN'"
-if [ -z "$FN" ];
-then
-    echo "No package file found for listing in download section of BinTray."
-else
-    $CURL -X PUT -d '{ "list_in_downloads":true }' \
-         "https://api.bintray.com/file_metadata/sasfit/development/$FN"
-    echo
-fi
+# Asynchronously publishes all unpublished content for a user’s package version.
+# https://www.jfrog.com/confluence/display/BT/Bintray+REST+API#BintrayRESTAPI-Publish/DiscardUploadedContent
+printf "Publish all unpublished content: "
+$CURL -X POST -d '{ "publish_wait_for_secs": -1 }' -w " HTTP Status: %{http_code}\n" \
+      "$API_URL/content/$PKG_PATH/$latest_ver/publish"
+sleep 1 # wait a moment, BinTray does not seem to publish immediately
+
+# Get available files on BinTray
+# https://www.jfrog.com/confluence/display/BT/Bintray+REST+API#BintrayRESTAPI-GetPackageFiles
+# for debugging:
+echo "Files on BinTray and 'published' status:"
+$CURL -X GET "$API_URL/packages/$PKG_PATH/versions/$latest_ver/files?include_unpublished=1" | python3 -c 'import sys, json; [print(" ", elem["name"], elem["published"]) for elem in json.loads(sys.stdin.readline())]'
+
+# Add uploaded files to download list, only possible for 'published' files
+for fn in $($CURL -X GET "$API_URL/packages/$PKG_PATH/versions/$latest_ver/files?include_unpublished=1" | python3 -c 'import sys, json; [print(elem["name"]) for elem in json.loads(sys.stdin.readline())]'); do
+    printf "Add uploaded $fn to direct download list:\n  "
+    $CURL -X PUT -d '{ "list_in_downloads":true }' -w " HTTP Status: %{http_code}\n" \
+         "$API_URL/file_metadata/sasfit/development/$fn"
+done
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
