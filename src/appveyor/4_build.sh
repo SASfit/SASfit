d6ce82ab338c60d24c9cbc3d60bf3411fd05a0cd
Ingo Breßler <dev@ingobressler.net>
2020-04-07 16:40:05 +0200

    appveyor: updated OS detection in build script

--- src/appveyor/4_build.sh
+++ src/appveyor/4_build.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
 echo
-echo "## Starting build ..."
+echo "## Starting build on '$(uname -s)' ..."
 echo
-echo "uname -s: '$(uname -s)'"
 
 NUM_LOGICAL_CORES=1
 CMAKE_GENERATOR='Unix Makefiles'
-if [ "$(uname -s)" = "Darwin" ]; # macOS
+if [ "$(uname -s)" = "Darwin" ];
 then
+    # macOS
     # determine number of computing cores
     NUM_LOGICAL_CORES=$(sysctl -n hw.ncpu)
     # find latest gcc and g++ compilers and set them as global variables
     export CC=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^gcc-(mp-)?[0-9]+' | tail -n1))
     export CXX=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^g\+\+-(mp-)?[0-9]+' | tail -n1))
-else # Windows
+
+elif uname -s | grep -qi '^mingw64';
+then
+    # Windows
+
     WMIC="$(command -v wmic)"
     if [ -x "$WMIC" ]; then
         NUM_LOGICAL_CORES="$($WMIC cpu get /format:list \
@@ -25,7 +29,8 @@ else # Windows
     CMAKE_GENERATOR='MSYS Makefiles'
 fi
 export NUM_LOGICAL_CORES
-echo "$0 determined $NUM_LOGICAL_CORES logical cores."
+echo "Determined $NUM_LOGICAL_CORES logical cores."
+echo
 
 cd "$APPVEYOR_BUILD_FOLDER" && \
 mkdir build && cd build && cmake -G "$CMAKE_GENERATOR" ../src && make -j$NUM_LOGICAL_CORES
