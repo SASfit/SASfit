cdd5bb10297f54854936eeb0f6b432a8b484edb7
Ingo Breßler <dev@ingobressler.net>
2020-05-18 10:52:27 +0200

    appveyor: updated build script for Linux

--- src/appveyor/4_build.sh
+++ src/appveyor/4_build.sh
@@ -6,19 +6,9 @@ echo
 
 NUM_LOGICAL_CORES=1
 CMAKE_GENERATOR='Unix Makefiles'
-if [ "$(uname -s)" = "Darwin" ];
-then
-    # macOS
-    # determine number of computing cores
-    NUM_LOGICAL_CORES=$(sysctl -n hw.ncpu)
-    # find latest gcc and g++ compilers and set them as global variables
-    export CC=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^gcc-(mp-)?[0-9]+' | tail -n1))
-    export CXX=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^g\+\+-(mp-)?[0-9]+' | tail -n1))
 
-elif uname -s | grep -qi '^mingw64';
+if uname -s | grep -qi '^mingw64'; # Windows
 then
-    # Windows
-
     WMIC="$(command -v wmic)"
     if [ -x "$WMIC" ]; then
         NUM_LOGICAL_CORES="$($WMIC cpu get /format:list \
@@ -27,6 +17,17 @@ then
             | tr -d '[:space:]' )"
     fi
     CMAKE_GENERATOR='MSYS Makefiles'
+else # macOS or Linux
+    # determine number of computing cores
+    if [ "$(uname -s)" = "Darwin" ]; then # on macOS
+        NUM_LOGICAL_CORES="$(sysctl -n hw.ncpu)"
+    else # on Linux
+        NUM_LOGICAL_CORES="$(awk '/processor/' /proc/cpuinfo | wc -l)"
+    fi
+    # find latest gcc and g++ compilers and set them as global variables
+    export CC=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^gcc-(mp-)?[0-9]+' | tail -n1))
+    export CXX=$(which $(echo $PATH | tr ':' '\n' | xargs -n 1 ls -1 | egrep '^g\+\+-(mp-)?[0-9]+' | tail -n1))
+
 fi
 export NUM_LOGICAL_CORES
 echo "Determined $NUM_LOGICAL_CORES logical cores."
