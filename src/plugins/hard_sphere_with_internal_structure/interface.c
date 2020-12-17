acb8c4c018fa1764d99d1d5dcd11c72c0767360d
kohlbrecher <joachim.kohlbrecher@psi.ch>
2020-12-17 17:12:23 +0100

    parametrization of PY-HS with an additional shell parameter

---
+++ src/plugins/hard_sphere_with_internal_structure/interface.c
@@ -0,0 +1,33 @@
+/*
+ * Author(s) of this file:
+ *   <your name> (<email address>)
+ */
+
+#ifdef MAKE_SASFIT_PLUGIN
+#include "include/private.h"
+
+// functions to mark for export
+SASFIT_PLUGIN_EXP_BEGIN(1)
+SASFIT_PLUGIN_EXP_ADD( sq_hard_sphere___py_3d_ )
+
+SASFIT_PLUGIN_EXP_END
+
+// import functions from other plugins
+SASFIT_PLUGIN_IMP_BEGIN( 6 )
+SASFIT_PLUGIN_IMP_ADD( sq_hard_sphere__4_3_ )
+SASFIT_PLUGIN_IMP_ADD( sq_hard_sphere__cs_ )
+SASFIT_PLUGIN_IMP_ADD( sq_hard_sphere__gh_ )
+SASFIT_PLUGIN_IMP_ADD( sq_hard_sphere__lhr_ )
+SASFIT_PLUGIN_IMP_ADD( sq_hard_sphere__mv_ )
+SASFIT_PLUGIN_IMP_ADD( sq_hard_sphere__py_ )
+SASFIT_PLUGIN_IMP_END
+
+SASFIT_PLUGIN_INTERFACE
+
+void do_at_init(void)
+{
+	// insert custom init code here (optional)
+}
+
+#endif
+
