c355245b5a1c0354a1f9523557a00b17568a665b
Ingo Breßler <dev@ingobressler.net>
2021-07-05 14:52:31 +0200

    appveyor: figuring out test syntax for all platforms

--- src/appveyor/7_testing.sh
+++ src/appveyor/7_testing.sh
@@ -5,7 +5,7 @@ testIfExists()
 {
     local fn; fn="$1"
     local dscr; dscr="$2"
-    echo "Testing for $dscr $fn:: $(if [ -f "$fn" ]; then echo "success!"; else echo "failure!"; fi)"
+    echo "Testing for $dscr $fn: $(if [ -f "$fn" ]; then echo "success!"; else echo "failure!"; fi)"
 }
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
