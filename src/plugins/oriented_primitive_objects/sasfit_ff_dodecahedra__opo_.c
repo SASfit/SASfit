cfa4a9a7a8cf77b2abea94acdc0c8a2310ad61be
Joachim Kohlbrecher <joachim.kohlbrecher@psi.ch>
2022-04-08 22:41:53 +0200

    first draft for dodecahedra formfactor

--- src/plugins/oriented_primitive_objects/sasfit_ff_dodecahedra__opo_.c
+++ src/plugins/oriented_primitive_objects/sasfit_ff_dodecahedra__opo_.c
@@ -5,11 +5,32 @@
 
 #include "include/private.h"
 #include <sasfit_error_ff.h>
+#include "complex.h"
 
 // define shortcuts for local parameters/variables
 
 scalar sasfit_ff_dodecahedra__opo_(scalar q, sasfit_param * param)
 {
+	opo_data opod;
+	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
+
+	SASFIT_CHECK_COND1((q < 0.0), param, "q(%lg) < 0",q);
+	SASFIT_CHECK_COND1((A <= 0.0), param, "a(%lg) <= 0",A); // modify condition to your needs
+	SASFIT_CHECK_COND1((B <= 0.0), param, "b(%lg) <= 0",B); // modify condition to your needs
+	SASFIT_CHECK_COND1((C <= 0.0), param, "c(%lg) <= 0",C); // modify condition to your needs
+
+	// insert your code here
+	SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(opod.ea,EA_X,EA_Y,EA_Z),0.0),param,"vector [EA_X,EA_Y,EA_Z] must have a norm != 0");
+	SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(opod.eb,EB_X,EB_Y,EB_Z),0.0),param,"vector [EB_X,EB_Y,EB_Z] must have a norm != 0");
+    SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(opod.ec,EC_X,EC_Y,EC_Z),0.0),param,"vector [EC_X,EC_Y,EC_Z] must have a norm != 0");
+
+    return gsl_pow_2(sasfit_ff_dodecahedra__opo__f(q,param));
+}
+
+scalar sasfit_ff_dodecahedra__opo__f(scalar q, sasfit_param * param)
+{
+    scalar Qx,Qy,Qz;
+    double complex FRD;
     scalar psi;
 	opo_data opod;
 	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
@@ -18,7 +39,6 @@ scalar sasfit_ff_dodecahedra__opo_(scalar q, sasfit_param * param)
 	SASFIT_CHECK_COND1((A <= 0.0), param, "a(%lg) <= 0",A); // modify condition to your needs
 	SASFIT_CHECK_COND1((B <= 0.0), param, "b(%lg) <= 0",B); // modify condition to your needs
 	SASFIT_CHECK_COND1((C <= 0.0), param, "c(%lg) <= 0",C); // modify condition to your needs
-	SASFIT_CHECK_COND1((H_R > tan(TILT)&&tan(TILT)!=0&&!gsl_isnan(tan(TILT))), param, "H/R > tan(TILT) (%lg)",tan(TILT));
 
 	// insert your code here
 	SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(opod.ea,EA_X,EA_Y,EA_Z),0.0),param,"vector [EA_X,EA_Y,EA_Z] must have a norm != 0");
@@ -33,14 +53,26 @@ scalar sasfit_ff_dodecahedra__opo_(scalar q, sasfit_param * param)
     opo_setEulerAngles(&opod,ALPHA,BETA,GAMMA);
     opo_init(&opod);
 
-}
+    opod.Qmod = q;
+    psi=sasfit_param_override_get_psi(PSI_DEG*M_PI/180.);
+    opod.Q[0] = q*cos(psi);
+    opod.Q[1] = q*sin(psi);
+    opod.Q[2] = 0;
+    opo_setQhat(&opod);
 
-scalar sasfit_ff_dodecahedra__opo__f(scalar q, sasfit_param * param)
-{
-	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
-
-	// insert your code here
-	return 0.0;
+    Qx = opod.Qhat[0];
+    Qy = opod.Qhat[1];
+    Qz = opod.Qhat[2];
+    H_R = 1;
+    param->p[18]=atan(1); // this is TILT
+    FRD =  (opo_ReFpyramid4(Qx,Qy, Qz,param) + I*opo_ImFpyramid4(Qx,Qy, Qz,param)) * cexp( I*Qz)
+          +(opo_ReFpyramid4(Qx,Qy,-Qz,param) + I*opo_ImFpyramid4(Qx,Qy,-Qz,param)) * cexp(-I*Qz)
+          +(opo_ReFpyramid4(Qx,Qz, Qy,param) + I*opo_ImFpyramid4(Qx,Qz, Qy,param)) * cexp( I*Qy)
+          +(opo_ReFpyramid4(Qx,Qz,-Qy,param) + I*opo_ImFpyramid4(Qx,Qz,-Qy,param)) * cexp(-I*Qy)
+          +(opo_ReFpyramid4(Qz,Qy, Qx,param) + I*opo_ImFpyramid4(Qz,Qy, Qx,param)) * cexp( I*Qx)
+          +(opo_ReFpyramid4(Qz,Qy,-Qx,param) + I*opo_ImFpyramid4(Qz,Qy,-Qx,param)) * cexp(-I*Qx)
+          +opo_sinc(Qx)*opo_sinc(Qy)*opo_sinc(Qz);
+    return (ETA_P-ETA_M) *opod.detDinv*cabs(FRD);
 }
 
 scalar sasfit_ff_dodecahedra__opo__v(scalar q, sasfit_param * param, int dist)
--- src/plugins/oriented_primitive_objects/sasfit_ff_dodecahedra__opo_random_.c
+++ src/plugins/oriented_primitive_objects/sasfit_ff_dodecahedra__opo_random_.c
@@ -5,18 +5,48 @@
 
 #include "include/private.h"
 #include <sasfit_error_ff.h>
+#include "complex.h"
 
 // define shortcuts for local parameters/variables
+
+opo_data dh_opod;
+
+scalar sasfit_ff_dodecahedra_opo_kernel_f(scalar theta, scalar phi, sasfit_param * param) {
+    dh_opod.Q[0] = dh_opod.Qmod*cos(phi)*sin(theta);
+    dh_opod.Q[1] = dh_opod.Qmod*sin(phi)*sin(theta);
+    dh_opod.Q[2] = dh_opod.Qmod         *cos(theta);
+    opo_setQhat(&dh_opod);
+    return (ETA_P-ETA_M) *dh_opod.detDinv*opo_FOH(&dh_opod);
+}
+scalar sasfit_ff_dodecahedra_opo_kernel(scalar theta, scalar phi, sasfit_param * param) {
+    return gsl_pow_2(sasfit_ff_dodecahedra_opo_kernel_f(theta,phi,param));
+}
 
 scalar sasfit_ff_dodecahedra__opo_random_(scalar q, sasfit_param * param)
 {
 	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
 
-	SASFIT_CHECK_COND1((q < 0.0), param, "q(%lg) < 0",q);
-	SASFIT_CHECK_COND1((A < 0.0), param, "a(%lg) < 0",A); // modify condition to your needs
+	SASFIT_CHECK_COND1((q < 0.0), param, "q(%lg) < 0",q); // modify condition to your needs
+	SASFIT_CHECK_COND1((A <= 0.0), param, "a(%lg) <= 0",A); // modify condition to your needs
+	SASFIT_CHECK_COND1((B <= 0.0), param, "b(%lg) <= 0",B); // modify condition to your needs
+	SASFIT_CHECK_COND1((C <= 0.0), param, "c(%lg) <= 0",C); // modify condition to your needs
 
 	// insert your code here
-	return 0.0;
+	SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(dh_opod.ea,EA_X,EA_Y,EA_Z),0.0),param,"vector [EA_X,EA_Y,EA_Z] must have a norm != 0");
+	SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(dh_opod.eb,EB_X,EB_Y,EB_Z),0.0),param,"vector [EB_X,EB_Y,EB_Z] must have a norm != 0");
+    SASFIT_CHECK_COND(SASFIT_EQUAL(opo_set_e(dh_opod.ec,EC_X,EC_Y,EC_Z),0.0),param,"vector [EC_X,EC_Y,EC_Z] must have a norm != 0");
+
+	dh_opod.a = A;
+	dh_opod.b = B;
+	dh_opod.c = C;
+    dh_opod.Rotation.convention = yaw_pitch_roll;
+    opo_setEulerAngles(&dh_opod,0,0,0);
+    opo_init(&dh_opod);
+
+    SASFIT_CHECK_COND(SASFIT_EQUAL(dh_opod.detDinv,0.0),param,"vectors ea, eb, ec seem to be linear dependent");
+
+    dh_opod.Qmod = q;
+    return sasfit_orient_avg(&sasfit_ff_dodecahedra_opo_kernel,param);
 }
 
 scalar sasfit_ff_dodecahedra__opo_random__f(scalar q, sasfit_param * param)
