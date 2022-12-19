29c7c0de01d9101fb0844cd7b151c9a2a56c2436
kohlbrecher <joachim.kohlbrecher@psi.ch>
2022-12-19 23:19:19 +0100

    using now sasfit_integrate in straight superhelix plugin

--- src/plugins/helix/sasfit_ff_superhelices_straight.c
+++ src/plugins/helix/sasfit_ff_superhelices_straight.c
@@ -25,30 +25,15 @@
 #define GAM2 param->p[MAXPAR-5]
 #define ALPHA	param->p[MAXPAR-6]
 
-scalar Kernel_w(scalar w, void * pam) {
+scalar Kernel_w(scalar w, sasfit_param *param) {
 	scalar Psi;
-	sasfit_param *param;
-	param = (sasfit_param *) pam;
-
-	Psi = sqrt(2.*R1*R1*(1.-cos(w/sqrt(R1*R1+A*A)))+A*A*w*w/(R1*R1+A*A)); 
+	Psi = sqrt(2.*R1*R1*(1.-cos(w/sqrt(R1*R1+A*A)))+A*A*w*w/(R1*R1+A*A));
 //	sasfit_out("a:%lg\t P:%lg\t R:%lg\t H:%lg\t L:%lg\t q:%lg\t\n",A,P,R1,H,L,Q);
-	return 2./(L*L) * (L-w)*sinc(Q*Psi);
-}
-
-int Kernel_straight(unsigned ndim, const double *x, void *pam,
-      unsigned fdim, double *fval) {
-	fval[0] = Kernel_w(x[0], pam);
-	return 0;
+	return 2. * (L-w)*sinc(Q*Psi);
 }
 
 scalar sasfit_ff_superhelices_straight(scalar q, sasfit_param * param)
 {
-	scalar sum,err, *aw;
-	scalar cubxmin[1], cubxmax[1], fval[1], ferr[1];
-	int lenaw, intstrategy;
-	
-	lenaw=4000;
-	
 	SASFIT_ASSERT_PTR(param); // assert pointer param is valid
 
 	SASFIT_CHECK_COND1((q < 0.0), param, "q(%lg) < 0",q);
@@ -60,48 +45,8 @@ scalar sasfit_ff_superhelices_straight(scalar q, sasfit_param * param)
 	Q=q;
 	A = P/(2.*M_PI);
 	L=H/A*sqrt(R1*R1+A*A);
-    intstrategy = sasfit_get_int_strategy();
-	intstrategy=P_CUBATURE;
-	switch(intstrategy) {
-    case OOURA_DOUBLE_EXP_QUADRATURE: {
-			aw = (scalar *)malloc((lenaw)*sizeof(scalar));
-			sasfit_intdeini(lenaw,GSL_DBL_MIN, sasfit_eps_get_nriq(), aw);
-			sasfit_intde(&Kernel_w, 0.0, L, aw, &sum, &err,param);
-			free(aw);
-            break;
-            } 
-    case OOURA_CLENSHAW_CURTIS_QUADRATURE: {
-			aw = (scalar *)malloc((lenaw+1)*sizeof(scalar));
-			sasfit_intccini(lenaw, aw);
-			sasfit_intcc(&Kernel_w, 0.0, L, sasfit_eps_get_nriq(), lenaw, aw, &sum, &err,param);
-			free(aw);
-            break;
-            } 
-	case H_CUBATURE: {
-			cubxmin[0]=0;
-			cubxmax[0]=L;
-			hcubature(1, &Kernel_straight,param,1, cubxmin, cubxmax, 
-				100000, 0.0, sasfit_eps_get_nriq(), ERROR_PAIRED, 
-				fval, ferr);
-			sum = fval[0];
-            break;
-            }
-    case P_CUBATURE: {	
-			cubxmin[0]=0;
-			cubxmax[0]=L;
-			pcubature(1, &Kernel_straight,param,1, cubxmin, cubxmax, 
-				100000, 0.0, sasfit_eps_get_nriq(), ERROR_PAIRED, 
-				fval, ferr);
-			sum = fval[0];
-            break;
-            }
-    default: {
-            sasfit_err("Unknown integration strategy\n");
-            break;
-            }
-	}
 
-	return sum*L*L;
+	return sasfit_integrate(0,L,&Kernel_w,param);
 }
 
 scalar sasfit_ff_superhelices_straight_f(scalar q, sasfit_param * param)
