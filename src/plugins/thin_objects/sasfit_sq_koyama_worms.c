27afb5aaef184a10c73fac135b285270bf2f34c1
kohlbrecher <joachim.kohlbrecher@psi.ch>
2023-01-07 18:06:30 +0100

    improved numerical stability of wormlike structure factor according to Koyama

--- src/plugins/thin_objects/sasfit_sq_koyama_worms.c
+++ src/plugins/thin_objects/sasfit_sq_koyama_worms.c
@@ -5,6 +5,7 @@
 
 #include "include/private.h"
 #include <sasfit_error_sq.h>
+#include <float.h>
 
 typedef struct
 {
@@ -30,7 +31,7 @@ scalar xg(scalar x, void *pam) {
 	rn2=cparam->rn2;
 	rn4=cparam->rn4;
 	K=cparam->K;
-	return sqrt((2.*rn2/gsl_pow_2(LB))*sqrt(10.*fabs(1.-3./5.*K)));
+	return sqrt((2.*rn2/gsl_pow_2(LB))*sqrt(10.*fabs(1.0-3./5.*K)));
 }
 
 scalar xf(scalar x, void *pam) {
@@ -46,7 +47,7 @@ scalar xf(scalar x, void *pam) {
 }
 
 scalar phi(scalar x, void *pam) {
-	scalar s,t;
+	scalar s,t,res;
 	sasfit_param * param;
 	koyama_param *cparam;
 	cparam = (koyama_param *) pam;
@@ -59,14 +60,22 @@ scalar phi(scalar x, void *pam) {
 	cparam->rn4=gsl_pow_4(LB)/4.*(5/3.*x*x - 52.0/9.*x-2./27.*(1.0-exp(-3.0*x))
 	           +8*(1.0-exp(-x))-2*x*exp(-x));
 
-
-	if (fabs(cparam->rn2) <= sasfit_eps_get_nriq()) {
-		cparam->K=1;
-	} else {
-		cparam->K=cparam->rn4/gsl_pow_2(cparam->rn2);
-	}
-//	sasfit_out("::phi: x: %lf, rn2=%lf, rn4=%lf, K=%lf\n",x, cparam->rn2,cparam->rn4,cparam->K);
-	return exp(-s*s/3.*xf(x,pam)) * gsl_sf_bessel_j0(fabs(s*xg(x,pam)));
+    if (fabs(x)< 1e-4) {
+        cparam->rn2=gsl_pow_2(LB)/2.*(0.5*gsl_pow_2(x)-gsl_pow_3(x)/6.0+gsl_pow_4(x)/24.-gsl_pow_5(x)/120.+gsl_pow_6(x)/720.);
+        cparam->rn4=gsl_pow_4(LB)/4.*(1./4.0*gsl_pow_4(x)-1./6.0*gsl_pow_5(x)+29./360.0*gsl_pow_6(x)-1./30.0*gsl_pow_7(x)+247./20160.0*gsl_pow_8(x));
+    } else {
+        cparam->rn2=gsl_pow_2(LB)/2.*(x-(1-exp(-x)));
+        cparam->rn4=gsl_pow_4(LB)/4.*(5/3.*x*x - 52.0/9.*x-2./27.*(1.0-exp(-3.0*x))
+	           +8*(1.0-exp(-x))-2*x*exp(-x));
+    }
+    if (fabs(x)<1e-12) {
+        cparam->K=1;
+    } else {
+        cparam->K=cparam->rn4/gsl_pow_2(cparam->rn2);
+    }
+	res = exp(-s*s/3.0*xf(x,pam)) * gsl_sf_bessel_j0(fabs(s*xg(x,pam)));
+//	sasfit_out("::phi: x: %lg, rn2=%lg, rn4=%lg, K=%lg phi:%lg xf:%lg, xg:%lg\n",x, cparam->rn2,cparam->rn4,cparam->K,res,xf(x,pam),xg(x,pam));
+	return res;
 }
 
 scalar koyama_worm_kernel(scalar x, void *pam) {
@@ -80,7 +89,7 @@ scalar koyama_worm_kernel_cub(const double *x, size_t ndim, void *pam) {
 	cparam = (koyama_param *) pam;
 	if (ndim < 1) {
 		sasfit_err("false dimensions ndim:%d\n",ndim);
-		return -1;
+		return 0;
 	}
 	return koyama_worm_kernel(x[0],pam);
 }
@@ -104,8 +113,10 @@ scalar sasfit_sq_koyama_worms(scalar q, sasfit_param * param)
 	cparam.param = param;
 	cparam.s=(q*LB/2.);
 	cparam.X = 2*L/LB;
+	cubxmin[0]=DBL_EPSILON*cparam.X;
+	cubxmax[0]=(1-DBL_EPSILON)*cparam.X;
 	cubxmin[0]=0;
-	cubxmin[0]=cparam.X;
+	cubxmax[0]=cparam.X;
 
 	ierr = sasfit_cubature(1,cubxmin, cubxmax,&koyama_worm_kernel_cub,&cparam , sasfit_eps_get_nriq(),&res,&err);
 
