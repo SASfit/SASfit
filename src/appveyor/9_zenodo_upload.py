7c96bdc39e1d8901446ee5c57a20434991dcfeff
Ingo Breßler <dev@ingobressler.net>
2023-01-12 16:09:18 +0100

    AppVeyor.Zenodo: fixed number of packages still needed

--- src/appveyor/9_zenodo_upload.py
+++ src/appveyor/9_zenodo_upload.py
@@ -214,9 +214,10 @@ for file in draft['files']:
 
 draft = updateMeta(draft, newmeta, verbose=verbose)
 # check that all expected packages are present
-if all([any([(pckg in fn['filename'].lower())
-             for pckg in ("windows", "macos", "appimage", "source")])
-        for fn in draft['files']]):
+packages = ("windows", "macos", "appimage", "source")
+if len(draft['files']) == len(packages)
+    and all([any([(pckg in fn['filename'].lower()) for pckg in packages])
+             for fn in draft['files']]):
     # finalize this record
     published, code = makeRequest(post, draft['links']['self'], '/actions/publish',
                                   params=dict(access_token=token),
