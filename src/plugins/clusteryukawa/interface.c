ff9d09b91ee325e27e341bd40c4e04c68348a1f9
kohlbrecher <joachim.kohlbrecher@psi.ch>
2024-04-26 00:18:31 +0200

    solve issue #21

--- src/plugins/clusteryukawa/interface.c
+++ src/plugins/clusteryukawa/interface.c
@@ -13,23 +13,11 @@ SASFIT_PLUGIN_EXP_ADD( sq_clusteryukawa )
 SASFIT_PLUGIN_EXP_END
 
 // import functions from other plugins
-SASFIT_PLUGIN_IMP_BEGIN( 17 )
-SASFIT_PLUGIN_IMP_ADD( sq_cluster__linear_chain_ )
-SASFIT_PLUGIN_IMP_ADD( sq_cluster__tetrahedron_ )
-SASFIT_PLUGIN_IMP_ADD( sq_dlca_incl_nn_pertubation )
-SASFIT_PLUGIN_IMP_ADD( sq_freely_joined_chain_of_rods )
-SASFIT_PLUGIN_IMP_ADD( sq_hcp_iso_gaussian )
-SASFIT_PLUGIN_IMP_ADD( sq_hcp_iso_lorentzian )
-SASFIT_PLUGIN_IMP_ADD( sq_hcp_iso_pearson )
-SASFIT_PLUGIN_IMP_ADD( sq_macro_ion_rmsa_1 )
-SASFIT_PLUGIN_IMP_ADD( sq_macro_ion_rmsa_2 )
-SASFIT_PLUGIN_IMP_ADD( sq_macro_ion_rmsa_3 )
-SASFIT_PLUGIN_IMP_ADD( sq_one_d_hard_spheres_random_orient )
+SASFIT_PLUGIN_IMP_BEGIN( 5 )
 SASFIT_PLUGIN_IMP_ADD( sq_paracrystal_1d_random )
 SASFIT_PLUGIN_IMP_ADD( sq_paracrystal_orig_1d_random )
 SASFIT_PLUGIN_IMP_ADD( sq_pc_random_flight )
 SASFIT_PLUGIN_IMP_ADD( sq_simple_sq_power_law )
-SASFIT_PLUGIN_IMP_ADD( sq_two_yukawa )
 SASFIT_PLUGIN_IMP_ADD( sq_one_yukawa )
 SASFIT_PLUGIN_IMP_END
 
