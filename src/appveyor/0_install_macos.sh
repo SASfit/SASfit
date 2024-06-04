0f9d95645454223ac8583641ee66b42d516b840f
Ingo Breßler <dev@ingobressler.net>
2024-06-04 15:53:34 +0200

    CI.macOS: Choose the gcc version freely again
    - should become gcc-14 at the moment

--- src/appveyor/0_install_macos.sh
+++ src/appveyor/0_install_macos.sh
@@ -12,7 +12,7 @@ set -x
 
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 brew update
-brew install --overwrite cmake gcc@13 gsl fftw zlib # GCC for xquartz libstc++ compat.
+brew install --overwrite cmake gcc gsl fftw zlib # GCC for xquartz libstc++ compat.
 brew install --cask xquartz
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
--- src/appveyor/4_build.sh
+++ src/appveyor/4_build.sh
@@ -25,8 +25,8 @@ else # macOS or Linux
     # determine number of computing cores
     if [ "$(uname -s)" = "Darwin" ]; then # on macOS
         NUM_LOGICAL_CORES="$(sysctl -n hw.ncpu)"
-        export CC=/usr/local/bin/gcc-13
-        export CXX=/usr/local/bin/g++-13
+        #export CC=/usr/local/bin/gcc-13
+        #export CXX=/usr/local/bin/g++-13
     else # on Linux
         NUM_LOGICAL_CORES="$(awk '/processor/' /proc/cpuinfo | wc -l)"
     fi
