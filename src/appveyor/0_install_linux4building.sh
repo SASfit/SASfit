e542259f87d61daab391af01ed32823be1c3e445
Ingo Breßler <dev@ingobressler.net>
2024-07-25 16:23:02 +0200

    CI.Linux: remove bintray repo sources only if found in sources.lists

--- src/appveyor/0_install_linux4building.sh
+++ src/appveyor/0_install_linux4building.sh
@@ -12,7 +12,8 @@ set -x
 
 # cleaning up unused package sources first
 sudo ls -la /etc/apt/sources.list.d/
-sudo sed -i -e '/bintray.com/d' /etc/apt/sources.list
+grep -q 'bintray.com' /etc/apt/sources.list && \
+    sudo sed -i -e '/bintray.com/d' /etc/apt/sources.list
 sudo apt-get update
 # extra compiler
 sudo apt-get -y install build-essential cmake zlib1g-dev libgsl-dev libfftw3-dev libx11-dev #gcc-14 g++-14 # for testing
