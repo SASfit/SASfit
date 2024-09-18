98d0aec45ab9d126904036b9c1f30ff3b6ed72f4
Ingo Breßler <ingo.bressler@bam.de>
2024-09-18 10:49:37 +0000

    CI@Linux: Show apt sources before installing extra packages

--- src/appveyor/0_install_linux4building.sh
+++ src/appveyor/0_install_linux4building.sh
@@ -14,6 +14,9 @@ set -x
 sudo ls -la /etc/apt/sources.list.d/
 grep -q 'bintray.com' /etc/apt/sources.list && \
     sudo sed -i -e '/bintray.com/d' /etc/apt/sources.list
+# Show all apt sources installed
+find /etc/apt -type f -path '*source*' -exec grep -H -v '^\(\([# ]\+\)\|$\)' {} \;
+# update the package database
 sudo apt-get update
 # extra compiler
 sudo apt-get -y install build-essential cmake zlib1g-dev libgsl-dev libfftw3-dev libx11-dev #gcc-14 g++-14 # for testing
