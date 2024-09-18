9553606eb9b5d0da408ce44ec6e32a89e44741fc
Ingo Breßler <ingo.bressler@bam.de>
2024-09-18 12:57:13 +0000

    CI@Linux: Format message output of install script

--- src/appveyor/0_install_linux4building.sh
+++ src/appveyor/0_install_linux4building.sh
@@ -5,29 +5,27 @@ echo "## Installing build tools on:"
 echo
 lsb_release -d -c
 echo
-
-# env | sort
-
-set -x
-
-echo
 echo "## Cleaning up unused package sources first"
 echo
+set -x
 sudo ls -la /etc/apt/sources.list.d/
 grep -q 'bintray.com' /etc/apt/sources.list && \
     sudo sed -i -e '/bintray.com/d' /etc/apt/sources.list
-
+set +x
 echo
 echo "## Show all apt sources installed"
 echo
-find /etc/apt -type f -path '*source*' -exec grep -H -v '^\(\([# ]\+\)\|$\)' {} \;
-
+set -x
+for fn in $(find /etc/apt -type f -path '*source*' ); do echo "## $fn:"; grep -v '^\(\([# ]\+\)\|$\)' $fn; done
+set +x
 echo
 echo "## Update the package database"
 echo
+set -x
 sudo apt-get update
-
+set +x
 echo
 echo "## Install extra development packages"
 echo
+set -x
 sudo apt-get -y install build-essential cmake zlib1g-dev libgsl-dev libfftw3-dev libx11-dev #gcc-14 g++-14 # for testing
