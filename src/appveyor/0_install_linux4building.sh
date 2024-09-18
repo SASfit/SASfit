b8b862fc430aa4b0c16160bbf51d86d73998a2ec
Ingo Breßler <ingo.bressler@bam.de>
2024-09-18 11:11:41 +0000

    CI@Linux: More verbose messages during the pkg install script

--- src/appveyor/0_install_linux4building.sh
+++ src/appveyor/0_install_linux4building.sh
@@ -10,13 +10,24 @@ echo
 
 set -x
 
-# cleaning up unused package sources first
+echo
+echo "## Cleaning up unused package sources first"
+echo
 sudo ls -la /etc/apt/sources.list.d/
 grep -q 'bintray.com' /etc/apt/sources.list && \
     sudo sed -i -e '/bintray.com/d' /etc/apt/sources.list
-# Show all apt sources installed
+
+echo
+echo "## Show all apt sources installed"
+echo
 find /etc/apt -type f -path '*source*' -exec grep -H -v '^\(\([# ]\+\)\|$\)' {} \;
-# update the package database
+
+echo
+echo "## Update the package database"
+echo
 sudo apt-get update
-# extra compiler
+
+echo
+echo "## Install extra development packages"
+echo
 sudo apt-get -y install build-essential cmake zlib1g-dev libgsl-dev libfftw3-dev libx11-dev #gcc-14 g++-14 # for testing
