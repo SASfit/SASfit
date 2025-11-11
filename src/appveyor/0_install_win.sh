086fbc1647cd19f4b2850a5c8d4e862b307c3fab
Ingo Breßler <ingo.bressler@bam.de>
2025-11-11 16:48:48 +0100

    ci(Windows): testing package install script

--- src/appveyor/0_install_win.sh
+++ src/appveyor/0_install_win.sh
@@ -15,6 +15,7 @@ echo ## Installing required packages:
 pacman -Rns --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-libwinpthread mingw-w64-x86_64-winpthreads mingw-w64-x86_64-binutils mingw-w64-x86_64-gcc-libs
 
 # Install specific versions of some build packages
+cd "$APPVEYOR_BUILD_FOLDER"
 mkdir packages
 cd packages
 pacman -S --noconfirm curl gawk
